30410c3973e00e04365ee50e37ed1dbac0d5237a
fedecx96 <federico.cuccu2412@gmail.com>
2024-10-09 15:12:28 +0200

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+print("Crea il nome della tua band!")
+print("****************************\n")
+
+nomeCitta = input("Inserisci il nome della tua città: ")
+nomeAnimale = input("Inserisci il nome del tuo animale domestico: ")
+
+print("\nIl nome della tua band sarà", nomeAnimale + " " + nomeCitta + "!")
